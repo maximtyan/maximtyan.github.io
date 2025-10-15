482644780bf8d997fa7adda3b755dfe9559a8687
maximtyan <104823119+maximtyan@users.noreply.github.com>
2025-10-15 11:35:25 +0000

    Deploying to gh-pages from @ maximtyan/maximtyan.github.io@1f60fcd8f8e4892646106dd92434853ad9bef962 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "Publications",
-          description: "My journal publications, patents and conference presentations",
+          description: "My journal publications, patents, and conference presentations",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
