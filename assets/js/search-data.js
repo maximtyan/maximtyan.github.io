caab3d3c417d1783e2788d113525dd9724366989
maximtyan <104823119+maximtyan@users.noreply.github.com>
2025-10-16 10:47:06 +0000

    Deploying to gh-pages from @ maximtyan/maximtyan.github.io@ab8c351107e7dbf08cb3d2e689d58546b5ffce03 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-bio-cv",
+          title: "Bio/CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
