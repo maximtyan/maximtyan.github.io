c66f808778408fde59822bfd11088e191d7e8d0f
maximtyan <104823119+maximtyan@users.noreply.github.com>
2025-10-16 17:54:10 +0000

    Deploying to gh-pages from @ maximtyan/maximtyan.github.io@a46ca724313001b26c6a12efc2785987e35cc7d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bio-cv",
-          title: "Bio/CV",
+  },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
